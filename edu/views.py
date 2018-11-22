848c9a096b2f41253d8e217252c29017c4eea7f4
mohammad <mohammadrezashamsashkezary@gmail.com>
2018-11-22 15:50:30 +0330

    views and forms

--- edu/views.py
+++ edu/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 #
 # def signup(request):
 #     if request.method == 'POST':
@@ -17,6 +19,42 @@ from django.shortcuts import render, redirect
 #     else:
 #         form = UserCreationForm()
 #     return render(request, 'signup.html', {'form': form})
+from forms.user_forms import SignUpForm
+
 
 def index(request):
     return render(request,"index.html")
+
+# def signup(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         # Redirect to a success page.
+#         ...
+#     else:
+#         # Return an 'invalid login' error message.
+
+def signup(request):
+    comments_list = User.objects.order_by('-created_at')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        print(form.data)
+        if form.is_valid():
+            user = form.save()
+            user_info = User()
+            user_info.user = user
+            user_info.is_active = True
+            user_info.save()
+            user.save()
+
+
+    else:
+        form = SignUpForm()
+    print(form.errors)
+    return render(request, 'sign_up.html', {'comments': comments_list,'form': form})
+
+
+def logout_view(request):
+    logout(request)
