88dac3358e74e8f12562295f764b7a4e96893301
mhno <makhsoosi7@gmail.com>
2018-11-23 14:02:28 +0330

    dsf

--- edu/views.py
+++ edu/views.py
@@ -67,13 +67,31 @@ def sign_up(request):
     # if request.method == 'POST':
     #     error = False
         if request.POST:
-            print(1)
             name=request.POST.get("name")
             lastname=request.POST.get("last_name")
             username = request.POST.get("username")
             email=request.POST.get("email")
             password1 = request.POST.get("password1")
             password2=request.POST.get("password2")
+            error1=False
+            error2=False
+            error3=False
+            if password1!=password2:
+                print("  ..1")
+                error1=True
+            if User.objects.filter(email=email).exists():
+                print("  ..2")
+                error2=True
+            if User.objects.filter(username=username).exists():
+                print("  ..3")
+                error3=True
+
+            if error1 or error2 or error3:
+                return render(request,"sign_up.html",{
+                    "error1" : error1,
+                    "error2" : error2,
+                    "error3" : error3,
+                })
 
             user = User.objects.create_user(username,email,password1)
 
